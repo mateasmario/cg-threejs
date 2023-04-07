c7c7f5b143b334166a04b20f6c961b8f2ef27e3c
mateasmario <mmmateas@gmail.com>
2023-04-07 13:27:32 +0300

    Added some models and directional light

--- main.js
+++ main.js
@@ -1,26 +1,46 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const scene  = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube     = new THREE.Mesh(geometry, material);
-scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 0;
+
+const loader = new GLTFLoader();
+
+loader.load( 'models/mrkrabs/scene.gltf', function ( gltf ) {
+    const model = gltf.scene;
+
+    model.position.setX(0);
+    model.position.setY(0);
+    model.position.setZ(-1000);
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+
+
+const light = new THREE.DirectionalLight( 0xffffff, 3 ); // soft white light
+light.position.setX(0);
+light.position.setY(100);
+light.position.setZ(1000);
+scene.add( light );
 
 function animate() {
-    requestAnimationFrame(animate);
-    
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    
-    renderer.render(scene, camera);
+	requestAnimationFrame( animate );
+
+	renderer.render( scene, camera );
 }
 
 animate();
