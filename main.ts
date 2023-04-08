7ccbb8c41d6837bd9a7f00aec2032538ea9d5cd0
mateasmario <mmmateas@gmail.com>
2023-04-08 10:46:02 +0300

    Added falling burgers + score

--- main.ts
+++ main.ts
@@ -26,6 +26,7 @@ camera.position.z = 100;
 const loader = new GLTFLoader();
 let mixer;
 let model;
+let model2;
 
 loader.load( 'models/mrkrabs/scene.gltf', function ( gltf ) {
     model = gltf.scene;
@@ -58,7 +59,6 @@ scene.add( light );
 
 // Clock
 const clock = new THREE.Clock()
-
 
 // Resize Window Event Listener
 window.addEventListener('resize', onWindowResize, false)
@@ -95,13 +95,13 @@ function setupKeyControls() {
             case 40: // Down Arrow Key
                 break;
             case 37: // Left Arrow Key
-                if (positionX > -500) {
+                if (positionX > -window.innerWidth/2) {
                     positionX -= 10;
                     model.position.setX(positionX);
                 }
                 break
             case 39: // Right Arrow Key
-                if (positionX < 500) {
+                if (positionX < window.innerWidth/2) {
                     positionX += 10;
                     model.position.setX(positionX);
                 }
@@ -110,10 +110,49 @@ function setupKeyControls() {
     }
 }
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+let meshArray: any[] = [];
+
+// Generate patties function
+async function generatePatties() {
+    while(true) {
+        loader.load( 'models/burger/scene.gltf', function ( gltf2 ) {
+            model2 = gltf2.scene;
+        
+            model2.position.setX(getRandomInt(-window.innerWidth/2, window.innerWidth/2));
+            model2.position.setY(window.innerHeight);
+            model2.position.setZ(-900);
+        
+            model2.scale.set(100, 100, 100);
+
+            scene.add( gltf2.scene );
+        
+        }, undefined, function ( error ) {
+        
+            console.error( error );
+        
+        } );
+
+        await delay(2000);
+
+        meshArray.push(model2);
+
+    }
+}
+
+// Score
+let score = 0;
+
 // Animate Function
 function animate() {
 	requestAnimationFrame( animate );
-    
+
     if (model) {
         if (model.rotation.y >= 0.5) {
             rotationDirection = "left";
@@ -128,6 +167,37 @@ function animate() {
         else {
             model.rotation.y -= 0.015;
         }
+
+
+        for (var i = 0; i<meshArray.length; i++) {
+            if (meshArray[i]) {
+                meshArray[i].position.setY(meshArray[i].position.y - 1); 
+                
+                // Delete if out of range
+                if (meshArray[i].position.y <= -window.innerHeight) {
+                    scene.remove(meshArray[i]);
+                    meshArray.splice(i, 1);
+                }
+
+                // Add score if in range
+                else if (meshArray[i].position.y <= -200) {
+                    const distance = Math.abs(meshArray[i].position.x - model.position.x);
+
+                    if (distance <= 100) {
+                        score++;
+
+                        var element = document.getElementById("info");
+
+                        if (element) {
+                            element.innerHTML = "Score: " + score;
+                        }
+
+                        scene.remove(meshArray[i]);
+                        meshArray.splice(i, 1);
+                    }
+                }
+            }
+        }
         
     }
 
@@ -139,6 +209,8 @@ function animate() {
 
 	render();
 }
+
+generatePatties();
 
 // Render Function
 function render() {
